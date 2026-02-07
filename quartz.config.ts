d54b053d7a941c8fd22bc626a604265799fd749a
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2026-02-07 09:46:45 -0300

    Quartz sync: Feb 7, 2026, 9:46 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,17 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Acúmulos",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    analytics: null,
+    locale: "pt-BR",
+    baseUrl: "acumulos.sergiohpreis.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "modified",
+    defaultDateType: "published",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,

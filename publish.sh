c57092f52f77188980dd9631b99170c8d2a96ed8
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2026-03-10 17:00:08 -0300

    Quartz sync: Mar 10, 2026, 5:00 PM

--- publish.sh
+++ publish.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh
 
+cd /Users/sergiohpreis/Documents/quartz
+
 # Remove a pasta e recria para evitar o prompt de confirmação do "rm *"
 rm -rf content
 mkdir content
 
 # Copia os arquivos
-cp -R ../brain/outbox/* content
+cp -R /Users/sergiohpreis/Documents/brain/outbox/* content
 
 # Roda o Quartz
 npx quartz sync
